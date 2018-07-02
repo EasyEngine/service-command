0a8914fe9d33ff432664af5258ee6c0bed5b3b6c
Kirtan Gajjar <kirtangajjar95@gmail.com>
2018-07-02 16:25:28 +0530

    Make changes to ee.yml

--- src/Service_Command.php
+++ src/Service_Command.php
@@ -15,33 +15,33 @@ use EE\Utils;
 
 class Service_Command extends EE_Command {
 
-	private $container_name = 'ee-nginx-proxy';
+	private $global_container_name = 'ee-nginx-proxy';
 	/**
 	 * Starts global reverse proxy container.
 	 */
 	public function start( $cmd, $descriptors = null ) {
-		\EE\Utils\default_launch( "docker start $this->container_name" );
+		\EE\Utils\default_launch( "docker start $this->global_container_name" );
 	}
 
 	/**
 	 * Stops global reverse proxy container.
 	 */
 	public function stop( $cmd, $descriptors = null ) {
-		\EE\Utils\default_launch( "docker stop $this->container_name" );
+		\EE\Utils\default_launch( "docker stop $this->global_container_name" );
 	}
 
 	/**
 	 * Restarts global reverse proxy container.
 	 */
 	public function restart( $cmd, $descriptors = null ) {
-		\EE\Utils\default_launch( "docker restart $this->container_name" );
+		\EE\Utils\default_launch( "docker restart $this->global_container_name" );
 	}
 
 	/**
 	 * Reloads global reverse proxy service without .
 	 */
 	public function reload( $cmd, $descriptors = null ) {
-		\EE\Utils\default_launch( "docker exec $this->container_name sh -c 'nginx -t && service nginx reload'" );
+		\EE\Utils\default_launch( "docker exec $this->global_container_name sh -c 'nginx -t && service nginx reload'" );
 	}
 
 }

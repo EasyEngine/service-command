94855ed7da2a63de0ae4071d332af8218a6d630f
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-09-25 17:43:59 +0530

    Update utility functions and add global-redis
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- src/helper/service-utills.php
+++ src/helper/service-utills.php
@@ -10,12 +10,14 @@ use Symfony\Component\Filesystem\Filesystem;
  * @throws EE\ExitException
  */
 function nginx_proxy_check() {
+
 	$proxy_type = EE_PROXY_TYPE;
-
 	if ( 'running' !== EE::docker()::container_status( $proxy_type ) ) {
-
-		$port_80_status  = EE\Site\Utils\get_curl_info( 'localhost', 80, true );
-		$port_443_status = EE\Site\Utils\get_curl_info( 'localhost', 443, true );
+		/**
+		 * Checking ports.
+		 */
+		$port_80_status  = get_curl_info( 'localhost', 80, true );
+		$port_443_status = get_curl_info( 'localhost', 443, true );
 
 		// if any/both the port/s is/are occupied.
 		if ( ! ( $port_80_status && $port_443_status ) ) {
@@ -24,18 +26,23 @@ function nginx_proxy_check() {
 
 			$fs = new Filesystem();
 
-			if ( ! $fs->exists( EE_ROOT_DIR . '/docker-compose.yml' ) ) {
-				self::generate_global_docker_compose_yml( $fs );
+			if ( ! $fs->exists( EE_CONF_ROOT . '/docker-compose.yml' ) ) {
+				generate_global_docker_compose_yml( $fs );
 			}
 
-			$EE_ROOT_DIR = EE_ROOT_DIR;
-			if ( ! EE::docker()::docker_network_exists( 'ee-global-network' ) ) {
-				if ( ! EE::docker()::create_network( 'ee-global-network' ) ) {
-					EE::error( 'Unable to create network ee-global-network' );
+			$EE_CONF_ROOT = EE_CONF_ROOT;
+			if ( ! EE::docker()::docker_network_exists( GLOBAL_BACKEND_NETWORK ) ) {
+				if ( ! EE::docker()::create_network( GLOBAL_BACKEND_NETWORK ) ) {
+					EE::error( 'Unable to create network ' . GLOBAL_BACKEND_NETWORK );
 				}
 			}
-			if ( EE::docker()::docker_compose_up( EE_ROOT_DIR, [ 'nginx-proxy' ] ) ) {
-				$fs->dumpFile( "$EE_ROOT_DIR/services/nginx-proxy/conf.d/custom.conf", file_get_contents( EE_ROOT . '/templates/custom.conf.mustache' ) );
+			if ( ! EE::docker()::docker_network_exists( GLOBAL_FRONTEND_NETWORK ) ) {
+				if ( ! EE::docker()::create_network( GLOBAL_FRONTEND_NETWORK ) ) {
+					EE::error( 'Unable to create network ' . GLOBAL_FRONTEND_NETWORK );
+				}
+			}
+			if ( EE::docker()::docker_compose_up( EE_CONF_ROOT, [ 'global-nginx-proxy' ] ) ) {
+				$fs->dumpFile( "$EE_CONF_ROOT/nginx/conf.d/custom.conf", file_get_contents( EE_ROOT . '/templates/custom.conf.mustache' ) );
 				EE::success( "$proxy_type container is up." );
 			} else {
 				EE::error( "There was some error in starting $proxy_type container. Please check logs." );
@@ -45,7 +52,35 @@ function nginx_proxy_check() {
 }
 
 /**
- * Generates global docker-compose.yml at EE_ROOT_DIR
+ * Function to start global conainer if it is not running.
+ *
+ * @param string $container Global container to be brought up.
+ */
+function init_global_container( $service, $container = '' ) {
+
+	if ( empty( $container ) ) {
+		$container = 'ee-' . $service;
+	}
+	if ( ! EE::docker()::docker_network_exists( GLOBAL_BACKEND_NETWORK ) ) {
+		if ( ! EE::docker()::create_network( GLOBAL_BACKEND_NETWORK ) ) {
+			EE::error( 'Unable to create network ' . GLOBAL_BACKEND_NETWORK );
+		}
+	}
+
+	$fs = new Filesystem();
+
+	if ( ! $fs->exists( EE_CONF_ROOT . '/docker-compose.yml' ) ) {
+		generate_global_docker_compose_yml( $fs );
+	}
+
+	if ( 'running' !== EE::docker()::container_status( $container ) ) {
+		chdir( EE_CONF_ROOT );
+		EE::docker()::boot_container( $container, 'docker-compose up -d ' . $container );
+	}
+}
+
+/**
+ * Generates global docker-compose.yml at EE_CONF_ROOT
  *
  * @param Filesystem $fs Filesystem object to write file
  */
@@ -55,7 +90,7 @@ function generate_global_docker_compose_yml( Filesystem $fs ) {
 	$data = [
 		'services' => [
 			[
-				'name'           => 'nginx-proxy',
+				'name'           => 'global-nginx-proxy',
 				'container_name' => EE_PROXY_TYPE,
 				'image'          => 'easyengine/nginx-proxy:' . $img_versions['easyengine/nginx-proxy'],
 				'restart'        => 'always',
@@ -68,18 +103,40 @@ function generate_global_docker_compose_yml( Filesystem $fs ) {
 					'LOCAL_GROUP_ID=' . posix_getegid(),
 				],
 				'volumes'        => [
-					EE_ROOT_DIR . '/services/nginx-proxy/certs:/etc/nginx/certs',
-					EE_ROOT_DIR . '/services/nginx-proxy/dhparam:/etc/nginx/dhparam',
-					EE_ROOT_DIR . '/services/nginx-proxy/conf.d:/etc/nginx/conf.d',
-					EE_ROOT_DIR . '/services/nginx-proxy/htpasswd:/etc/nginx/htpasswd',
-					EE_ROOT_DIR . '/services/nginx-proxy/vhost.d:/etc/nginx/vhost.d',
-					EE_ROOT_DIR . '/services/nginx-proxy/html:/usr/share/nginx/html',
+					EE_CONF_ROOT . '/nginx/certs:/etc/nginx/certs',
+					EE_CONF_ROOT . '/nginx/dhparam:/etc/nginx/dhparam',
+					EE_CONF_ROOT . '/nginx/conf.d:/etc/nginx/conf.d',
+					EE_CONF_ROOT . '/nginx/htpasswd:/etc/nginx/htpasswd',
+					EE_CONF_ROOT . '/nginx/vhost.d:/etc/nginx/vhost.d',
+					EE_CONF_ROOT . '/nginx/html:/usr/share/nginx/html',
 					'/var/run/docker.sock:/tmp/docker.sock:ro',
 				],
 				'networks'       => [
-					'global-network',
+					'global-frontend-network',
 				],
-
+			],
+			[
+				'name'           => GLOBAL_DB,
+				'container_name' => GLOBAL_DB_CONTAINER,
+				'image'          => 'easyengine/mariadb:' . $img_versions['easyengine/mariadb'],
+				'restart'        => 'always',
+				'environment'    => [
+					'MYSQL_ROOT_PASSWORD=' . \EE\Utils\random_password(),
+				],
+				'volumes'        => [ './app/db:/var/lib/mysql' ],
+				'networks'       => [
+					'global-backend-network',
+				],
+			],
+			[
+				'name'           => GLOBAL_REDIS,
+				'container_name' => GLOBAL_REDIS_CONTAINER,
+				'image'          => 'easyengine/redis:' . $img_versions['easyengine/redis'],
+				'restart'        => 'always',
+				'volumes'        => [ EE_CONF_ROOT . '/services/redis:/data' ],
+				'networks'       => [
+					'global-backend-network',
+				],
 			],
 			[
 				'name'           => 'elasticsearch',
@@ -96,30 +153,16 @@ function generate_global_docker_compose_yml( Filesystem $fs ) {
 					],
 				],
 				'volumes'        => [
-					EE_ROOT_DIR . 'services/elasticsearch:/usr/share/elasticsearch/data',
+					EE_CONF_ROOT . 'services/elasticsearch:/usr/share/elasticsearch/data',
 				],
 
 				'networks' => [
-					'global-network',
-				],
-			],
-			[
-				'name'           => GLOBAL_DB,
-				'container_name' => GLOBAL_DB_CONTAINER,
-				'image'          => 'easyengine/mariadb:' . $img_versions['easyengine/mariadb'],
-				'restart'        => 'always',
-				'environment'    => [
-					'MYSQL_ROOT_PASSWORD=' . \EE\Utils\random_password(),
-				],
-				'volumes'        => [ './app/db:/var/lib/mysql' ],
-				'networks'       => [
-					'global-network',
+					'global-backend-network',
 				],
 			],
 		],
-
 	];
 
 	$contents = EE\Utils\mustache_render( SERVICE_TEMPLATE_ROOT . '/global_docker_compose.yml.mustache', $data );
-	$fs->dumpFile( EE_ROOT_DIR . '/docker-compose.yml', $contents );
+	$fs->dumpFile( EE_CONF_ROOT . '/docker-compose.yml', $contents );
 }

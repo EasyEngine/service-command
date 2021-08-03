79207fe3356a9f2d2ae8742064f705e4f477a073
Kirtan Gajjar <kirtangajjar95@gmail.com>
2021-08-03 13:54:53 +0530

    Add subnet IP migration for services

---
+++ migrations/container/20210802191554_service-command_add_subnet_ip.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace EE\Migration;
+
+use EE;
+use function EE\Service\Utils\generate_global_docker_compose_yml;
+
+class AddSubnetIp extends Base {
+
+	public function __construct() {
+
+		parent::__construct();
+		if ( $this->is_first_execution || IS_DARWIN ) {
+			$this->skip_this_migration = true;
+		}
+	}
+
+	/**
+	 * Execute global service container name update.
+	 *
+	 * @throws EE\ExitException
+	 */
+	public function up() {
+
+		if ( $this->skip_this_migration ) {
+			EE::debug( 'Skipping add-subnet-ip migration as it is not needed.' );
+
+			return;
+		}
+
+		EE::runcommand( 'service refresh' );
+		EE::debug( 'Starting add-subnet-ip' );
+	}
+
+	/**
+	 * No need for down.
+	 *
+	 * @throws EE\ExitException
+	 */
+	public function down() {
+	}
+}

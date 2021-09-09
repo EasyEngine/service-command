fcb56b1635ccfc5c0da1b26e673b17168060c05c
Kirtan Gajjar <kirtangajjar95@gmail.com>
2021-09-09 13:54:27 +0530

    Fix migration network ip issue

--- migrations/container/20210802191554_service-command_add_subnet_ip.php
+++ migrations/container/20210802191554_service-command_add_subnet_ip.php
@@ -28,8 +28,54 @@ class AddSubnetIp extends Base {
 			return;
 		}
 
-		$service_command = new \Service_Command();
-		$service_command->refresh( [], [] );
+		$backend_containers  = EE::launch( 'docker network inspect -f \'{{ range $key, $value := .Containers }}{{ printf "%s\n" $value.Name}}{{ end }}\' ' . GLOBAL_BACKEND_NETWORK )->stdout;
+		$frontend_containers = EE::launch( 'docker network inspect -f \'{{ range $key, $value := .Containers }}{{ printf "%s\n" $value.Name}}{{ end }}\' ' . GLOBAL_FRONTEND_NETWORK )->stdout;
+
+		$backend_containers  = explode( "\n", $backend_containers );
+		$frontend_containers = explode( "\n", $frontend_containers );
+
+		EE::log( 'Disconnecting containers from global backend and frontend networks for network update.' );
+
+		foreach( $backend_containers as $container ) {
+			EE::launch( 'docker network disconnect ' . GLOBAL_BACKEND_NETWORK . ' ' . $container );
+		}
+		foreach( $frontend_containers as $container ) {
+			EE::launch( 'docker network disconnect ' . GLOBAL_FRONTEND_NETWORK . ' ' . $container );
+		}
+
+		// Backup names of all the running containers
+		$running_services = [];
+		$count            = 0;
+		$whitelisted_services = [ 'nginx-proxy', 'db', 'redis' ];
+		chdir( EE_SERVICE_DIR );
+
+		foreach ( $whitelisted_services as $service ) {
+			$running_services[ $count ]['name']  = $service;
+			$launch                              = EE::launch( 'docker-compose ps -q global-' . $service );
+			$running_services[ $count ]['state'] = $launch->stdout;
+			$count++;
+		}
+
+		// Remove the containers and (especially) networks
+		EE::launch( 'docker-compose down' );
+
+		\EE\Service\Utils\generate_global_docker_compose_yml( new \Symfony\Component\Filesystem\Filesystem() );
+
+		// Start all the previously running containers
+		foreach ( $running_services as $service ) {
+			if ( ! empty( $service['state'] ) ) {
+				EE::exec( \EE_DOCKER::docker_compose_with_custom() . " up -d global-${service['name']}", true, true );
+			}
+		}
+
+
+		EE::log( 'Reconnecting containers from global backend and frontend networks for network update.' );
+		foreach( $backend_containers as $container ) {
+			EE::launch( 'docker network connect ' . GLOBAL_BACKEND_NETWORK . ' ' . $container );
+		}
+		foreach( $frontend_containers as $container ) {
+			EE::launch( 'docker network connect ' . GLOBAL_FRONTEND_NETWORK . ' ' . $container );
+		}
 
 		EE::debug( 'Starting add-subnet-ip' );
 	}
